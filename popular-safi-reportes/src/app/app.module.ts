77be16afb9363bcd1296b64eb272d1f48a3f4380
PR154357 <privera@popular-safi.com>
2023-08-21 17:16:49 -0500

    Integracion Angular-Keycloak

---
+++ popular-safi-reportes/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8180/',
+        realm: 'PopularSAFI',
+        clientId: 'angular-client-reporte-usuarios'
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        flow: "standard",
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      },
+      loadUserProfileAtStartUp: true
+    });
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, KeycloakAngularModule
+  ],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
